0d89a4e02588aec6dbc6a8fd7e0fe9b3a1e71e52
thomas jack <694543965@qq.com>
2021-02-27 14:33:23 +0800

    Hi

--- script/quickTomcat.sh
+++ script/quickTomcat.sh
@@ -7,7 +7,6 @@ yum -y install java-1.8.0-openjdk*
 wget https://mirrors.cnnic.cn/apache/tomcat/tomcat-9/v9.0.43/bin/apache-tomcat-9.0.43.tar.gz
 tar -zxvf apache-tomcat-9.0.43.tar.gz
 mv apache-tomcat-9.0.43 /usr/local
-cd /usr/local/apache-tomcat-9.0.43/bin
 #firewall
 firewall-cmd --zone=public --add-port=80/tcp --permanent
 firewall-cmd --zone=public --add-port=80/udp --permanent
@@ -15,3 +14,7 @@ firewall-cmd --zone=public --add-port=8080/tcp --permanent
 firewall-cmd --zone=public --add-port=8080/udp --permanent
 firewall-cmd --reload
 firewall-cmd --zone=public --list-ports
+
+#launch tomcat
+cd /usr/local/apache-tomcat-9.0.43/bin
+./startup.sh
